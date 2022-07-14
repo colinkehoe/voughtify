dccf35529117bba27e4684f2a3869971dad84a41
colinkehoe <kehoecolin012@gmail.com>
2022-07-13 21:07:31 -0500

    Scotland Forever
    Add Scotland Forever command

---
+++ slash/scotlandforever.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { QueryType } = require('discord-player');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('scotlandforever')
+        .setDescription('Plays Scotland Forever a few times.')
+        .addNumberOption((option) => option.setName('number').setDescription('The number of times to play the song.').setRequired(false)),
+    run: async ({ client, interaction }) => {
+        if (!interaction.member.voice.channel) return interaction.editReply('You must be in a voice channel to use this command!');
+
+        const queue = await client.player.createQueue(interaction.guild, {
+            metadata: interaction.channel,
+            autoSelfDeaf: false
+        });
+
+        if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+
+        let embed = new MessageEmbed();
+
+        let url = process.env.SCOTLAND_FOREVER_URL;
+        let number = interaction.options.getNumber('number');
+        
+        let result = await client.player.search(url, {
+            requestedBy: interaction.user,
+            searchEngine: QueryType.SPOTIFY_SONG
+        });
+        if (result.tracks.length === 0) {
+            return interaction.editReply('There was an error!');
+        }
+        for (let i = 0; i < number; i++) {
+            let song = result.tracks[0];
+            await queue.addTrack(song);
+        }
+        embed
+            .setDescription(`**${song.author} -- [${song.title}](${song.url})** has been added to the Queue. \nRequested by <@${song.requestedBy.id}>`)
+            .setThumbnail(song.thumbnail)
+            .setFooter({ text: `Duration: ${song.duration}` });
+    }
+}
